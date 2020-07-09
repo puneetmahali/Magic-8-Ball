6f54a0a5b2b85b6ba06aef55cc513ec92a7c2667
Puneet Mahali <iospuneet@gmail.com>
2020-07-09 20:12:57 +0200

    Project Setup

---
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Puneet Mahali on 09.07.20.
+//  Copyright © 2020 Puneet Mahali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
